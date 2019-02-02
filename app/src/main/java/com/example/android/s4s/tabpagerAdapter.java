f95623fb1f47ece0eb8460c177aafae900c7c9b5
rg1107 <rahul.gite11@gmail.com>
2019-02-02 10:35:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/s4s/tabpagerAdapter.java
@@ -0,0 +1,67 @@
+package com.example.android.s4s;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class tabpagerAdapter extends FragmentStatePagerAdapter {
+
+    String[] tabarray = new String[]{"Computer Science","Mechanical","Electrical","Electronics","Information Technology","Civil","Mining","Metallurgy"};
+
+    Integer tabnumber = 8;
+
+    public tabpagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabarray[position];
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        switch(i)
+        {
+            case 0:
+                CS cs1 = new CS();
+                return cs1;
+
+            case 1:
+                ME  me1 = new ME();
+                return me1;
+
+            case 2:
+                EE ee1 = new EE();
+                return ee1;
+
+            case 3:
+                EC ec1 = new EC();
+                return ec1;
+
+            case 4:
+                IT it1 = new IT();
+                return it1;
+
+            case 5:
+                CV cv1 = new CV();
+                return cv1;
+
+            case 6:
+                MN mn1 = new MN();
+                return mn1;
+
+            case 7:
+                MT mt1 = new MT();
+                return mt1;
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return tabnumber;
+    }
+}
